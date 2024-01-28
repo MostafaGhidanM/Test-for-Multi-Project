10548dc33fc87bd1043d8a21780e56a02b02c357
samir-elhadad <samirhussenwork@gmail.com>
2024-01-28 17:44:11 +0200

    back

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\BannerController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/test', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
